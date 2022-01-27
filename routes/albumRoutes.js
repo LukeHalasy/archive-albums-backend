9acf83916f2d841df5f6d5bff1aa2d06fa81d6e7
LukeHalasy <lhalasy@gmail.com>
2022-01-26 21:50:21 -0500

    Setup basic authentication middleware

--- routes/albumRoutes.js
+++ routes/albumRoutes.js
@@ -1,12 +1,14 @@
 const express = require('express')
 
 const albumController = require("../controllers/albumController.js")
+const protect = require('../middleware/authMiddleware')
+
 const router = express.Router()
 
-router.route("/addAlbum").post(albumController.addAlbum);
-router.route("/deleteAlbum").delete(albumController.deleteAlbum);
-router.route("/deleteAll").delete(albumController.deleteAll);
-router.route("/getAlbums").get(albumController.getAlbums);
-router.route("/updateAlbumStatus").patch(albumController.updateAlbumStatus);
+router.route("/addAlbum").post(protect, albumController.addAlbum);
+router.route("/deleteAlbum").delete(protect, albumController.deleteAlbum);
+router.route("/deleteAll").delete(protect, albumController.deleteAll);
+router.route("/getAlbums").get(protect, albumController.getAlbums);
+router.route("/updateAlbumStatus").patch(protect, albumController.updateAlbumStatus);
 
 module.exports = router;
