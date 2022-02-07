59d2c45ccda65e0691fe13d5adbf29d3e4504fe0
LukeHalasy <lhalasy@gmail.com>
2022-02-07 15:10:05 -0500

    Swithced to email instead of username

--- models/userModel.js
+++ models/userModel.js
@@ -1,10 +1,18 @@
 const mongoose = require("mongoose")
 
 const userSchema = new mongoose.Schema({
-  username: {
+  email: {
     type: String,
-    required: [true, 'User must have a username'],
+    trim: true,
+    lowercase: true,
     unique: true,
+    validate: {
+     validator: function(v) {
+      return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(v);
+     },
+     message: "Please enter a valid email"
+    },
+    required: [true, "Email required"]
   },
   password: {
     type: String,
