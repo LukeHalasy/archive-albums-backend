c443b6ecfc18a8aa24f99d3444c8bddbc555ea1c
LukeHalasy <lhalasy@gmail.com>
2022-02-08 13:34:58 -0500

    Switched current user to get again

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,6 +7,6 @@ router.route("/").get(authController.test);
 router.route("/signup").post(authController.signUp);
 router.route("/login").post(authController.login);
 router.route("/logout").delete(authController.logout);
-router.route("/currentUser").post(authController.currentUser);
+router.route("/currentUser").get(authController.currentUser);
 
 module.exports = router;
